634925de467e13a850038a7fb21cc022ec926319
Javier Ribó <elribonazo@gmail.com>
2025-11-29 01:18:59 +0100

    fix: build script Signed-off-by: Javier Ribó <elribonazo@gmail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 cargo build -p kernel --target riscv64gc-unknown-none-elf --release
 cargo build -p relay --release
 cargo build -p riscv-vm --release
-cargo run -p fs -- --output fs/disk.img --dir fs/root --size 512
+cargo run -p fs -- --output target/riscv64gc-unknown-none-elf/release/disk.img --dir fs/root --size 512
 
 cd riscv-vm && yarn build && cd ..
